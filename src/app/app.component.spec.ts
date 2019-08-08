75f145b8f086fa46699186c3abe92bb2d01993bd
SailedApple1991 <dylanwang1991@gmail.com>
2019-08-07 23:48:42 -0400

    Add angular material support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-
+import { HeaderComponent } from './header/header.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        PostCreateComponent
+        PostCreateComponent,
+        HeaderComponent
       ],
     }).compileComponents();
   }));
