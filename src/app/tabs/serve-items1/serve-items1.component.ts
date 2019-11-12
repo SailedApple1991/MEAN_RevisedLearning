3275660ecfe4561eb7b8277983c773ed51a2019c
SailedApple1991 <dylanwang1991@gmail.com>
2019-11-12 01:30:07 -0500

    add subtitle

--- src/app/tabs/serve-items1/serve-items1.component.ts
+++ src/app/tabs/serve-items1/serve-items1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import data from './data.json';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 @Component({
   selector: 'app-serve-items1',
   templateUrl: './serve-items1.component.html',
@@ -11,8 +12,9 @@ export class ServeItems1Component implements OnInit {
   private id: string;
   private items = data;
   public item;
+  public description: SafeHtml;
 
-  constructor( private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor( private activatedRoute: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer) {
    }
    public picsList: any[] = [
     {image : '../assets/service/deeptissue.jpg'},
@@ -24,6 +26,9 @@ export class ServeItems1Component implements OnInit {
     this.activatedRoute.params.forEach((params: Params) => {
       this.id = params.id;
       this.item = this.items.find(tab => tab.id === this.id);
+      this.description = this.sanitizer.bypassSecurityTrustHtml(
+        this.item.description
+      )
       console.log(this.item, +'\n' );
     });
   }
